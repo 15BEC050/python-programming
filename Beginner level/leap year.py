179142b35be5fc7f8f209b5de6e2c91d76596d0a
15BEC050 <37368418+15BEC050@users.noreply.github.com>
2018-03-20 14:50:22 +0530

    Update leap year.py

--- Beginner level/leap year.py
+++ Beginner level/leap year.py
@@ -1,9 +1,9 @@
 a=int(input("enter the inpurt:"))
 if((a%4)==0):
-    print("leap")
+    print("yes")
 elif((a%100)==0):
-    print("leap")
+    print("yes")
 elif((a%400)==0):
-    print("leap")
+    print("yes")
 else:
-    print("not leap")
+    print("no")

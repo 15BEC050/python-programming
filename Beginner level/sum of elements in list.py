8a1290649a210ecc585a9e754624288394167612
15BEC050 <37368418+15BEC050@users.noreply.github.com>
2018-03-20 20:23:44 +0530

    Update sum of elements in list.py

--- Beginner level/sum of elements in list.py
+++ Beginner level/sum of elements in list.py
@@ -1,10 +1,10 @@
-arr = []
+input = []
 sum=0
-n=int(raw_input())
+s=int(raw_input())
 k=int(raw_input())
-for i in range(n):
+for i in range(s):
     a=int(raw_input())
-    arr.append(a)
+    input.append(a)
     print(a)
 for i in range(k):
     sum=sum + a

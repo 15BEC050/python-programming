bb75264ff6327798ef5161eb01ca2c81e4fc772b
15BEC050 <37368418+15BEC050@users.noreply.github.com>
2018-03-20 11:16:36 +0530

    Update largest of three numbers.py

--- Beginner level/largest of three numbers.py
+++ Beginner level/largest of three numbers.py
@@ -1,9 +1,9 @@
 a=int(input("enter the value:"))
 b=int(input("enter the value:"))
 c=int(input("enter the value:"))
-if(a>b and a>c):
+if(a>=b and a>=c):
     print(a)
-elif(b>a and b>c):
+elif(b>=a and b>=c):
     print(b)
 else:
     print(c)

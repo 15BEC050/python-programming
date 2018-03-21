20ca78847e4700bb5239352827e644affd80b003
15BEC050 <37368418+15BEC050@users.noreply.github.com>
2018-03-21 12:11:32 +0530

    Update prime number or not.py

--- Beginner level/prime number or not.py
+++ Beginner level/prime number or not.py
@@ -2,11 +2,11 @@ def main():
     a=int(input("enter the value:"))
     for i in range(2,a):
         if(a%i==0):
-            print("not  prime")
+            print("no")
             print(i ,"times", a//i ,"is", a )
             break
     else:
-        print("prime number",a)
+        print("yes")
 
 
 if __name__ == '__main__':
